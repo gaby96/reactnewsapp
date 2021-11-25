1b3a9245fec530de7e23b4b8c1c181b1f2747197
gaby96 <gabrieladdo64@gmail.com>
2021-11-25 08:11:39 +0000

    fixed bug

--- src/component/Appbar/Appbar.js
+++ src/component/Appbar/Appbar.js
@@ -24,7 +24,7 @@ export default function HeaderAppbar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} style={{color:'#ffffff'}}>
+          <Typography  component="div" sx={{ flexGrow: 1 }} style={{color:'#ffffff'}}>
             New York Times Most Popular Articles
           </Typography>
           
--- src/component/Appbar/NewsSection.js
+++ src/component/Appbar/NewsSection.js
@@ -86,7 +86,7 @@ export default function NewsSection() {
     )
 }
 
-module.exports = NewsSection;
+
 
 
 
